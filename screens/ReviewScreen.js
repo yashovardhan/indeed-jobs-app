3222f6fdfb6d4db3bfa7718ea4e0a057b20002fe
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-04 23:43:27 +0530

    ReviewScreen.js: Customise header using navigationOptions

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = props => {
+    const { navigate } = props.navigation;
+    return {
+      headerTitle: 'Review Jobs',
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0,122,255,1)"
+        />
+      ),
+      headerStyle: {
+        marginTop: Platform.OS === 'android' ? 24 : 0,
+      },
+    };
+  };
+
   render() {
     return (
       <View>
