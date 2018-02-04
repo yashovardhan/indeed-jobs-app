95c03d7932f88e256c266574946b58986065999b
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-04 23:58:25 +0530

    Slides.js: Add new component

---
+++ components/Slides.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Slides extends Component {
+  render() {
+    return (
+      <View>
+        <Text>a</Text>
+      </View>
+    );
+  }
+}
+
+export default Slides;
