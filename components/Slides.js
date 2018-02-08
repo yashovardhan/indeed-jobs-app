8e09e221aeebd3093632bf6c1e1984fb6a6a89bc
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-09 00:43:54 +0530

    WelcomeScreen complete

--- components/Slides.js
+++ components/Slides.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Onwards!"
+          raised
+          buttonStyle={styles.buttonStyle}
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
+
+  renderSlides() {
+    return this.props.data.map((slide, index) => {
+      return (
+        <View key={slide.text} style={[styles.slideStyle, { backgroundColor: slide.color }]}>
+          <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
+        </View>
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>a</Text>
-      </View>
+      <ScrollView horizontal style={{ flex: 1 }} pagingEnabled>
+        {this.renderSlides()}
+      </ScrollView>
     );
   }
 }
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+  },
+  textStyle: {
+    fontSize: 30,
+    color: 'white',
+    marginLeft: 10,
+    marginRight: 10,
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    backgroundColor: '#0288D1',
+    marginTop: 15,
+  },
+};
 
 export default Slides;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp', color: '#03A9F4' },
+  { text: 'Use this to get a job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4' },
+];
 
 class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth')
+  }
+
   render() {
-    return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
-    );
+    return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
   }
 }
 
