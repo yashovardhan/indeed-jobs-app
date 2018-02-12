3f3f830aaa6f477cb7925c3e14fcaecab6ee5980
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-12 13:12:02 +0530

    DeckScreen.js: Show a deck of jobs nearby

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card } from 'react-native-elements';
+
+import Swipe from '../components/Swipe';
+import * as actions from '../actions';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.045,
+      latitudeDelta: 0.02,
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android'}
+            initialRegion={initialRegion}
+          />
+        </View>
+
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <View style={{ height: 100 }}>
+          <Text>{job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}</Text>
+        </View>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return <Card title="No More Jobs" />;
+  }
+
   render() {
     return (
-      <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+      <View style={{ marginTop: 10 }}>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeRight={job => this.props.likeJob(job)}
+          keyProp="jobkey"
+        />
       </View>
     );
   }
 }
 
-export default DeckScreen;
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+  },
+};
+
+function mapStateToProps({ jobs }) {
+  return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps, actions)(DeckScreen);
