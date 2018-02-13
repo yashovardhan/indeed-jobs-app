457be6d7562220eee42d2d6ebc4da0f21d0947bc
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-13 12:04:55 +0530

    reducers/index.js: likes -> likedJobs

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth_reducer';
 import jobs from './jobs_reducer';
-import likes from './likes_reducer';
+import likedJobs from './likes_reducer';
 
 export default combineReducers({
   auth,
   jobs,
-  likes,
+  likedJobs,
 });
