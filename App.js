f4c7cfaf5e1392619cf547753ec4af16a28fb188
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-13 15:15:03 +0530

    App.js: Send push notification on componentDidMount

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+import { Notifications } from 'expo';
+import { Alert } from 'react-native';
 
+import registerForNotifications from './services/push_notifications'
 import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -11,6 +14,17 @@ import SettingsScreen from './screens/SettingsScreen';
 import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener(notification => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text) {
+        Alert.alert('New Push Notification', text, [{ text: 'Ok.' }]);
+      }
+    });
+  }
+
   render() {
     const MainNavigator = TabNavigator(
       {
