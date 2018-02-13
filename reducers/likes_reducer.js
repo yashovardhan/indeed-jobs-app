9ec53d7b3e914673a1ab5c1792c4e77c836506d6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-13 12:23:29 +0530

    likes_reducer.js: Add case CLEAR_LIKED_JOBS

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 
-import { LIKE_JOB } from '../actions/types';
+import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'jobkey');
+    case CLEAR_LIKED_JOBS:
+      return [];
     default:
       return state;
   }
