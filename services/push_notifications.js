23273712fc6d58373d84e2ebde6bbd12d1dbb5a6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-13 15:14:23 +0530

    Add push_notifications.js

---
+++ services/push_notifications.js
@@ -0,0 +1,22 @@
+import { Permissions, Notifications } from 'expo';
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+
+const PUSH_ENDPOINT = 'http://rallycoding.herokuapp.com/api/tokens';
+
+export default async () => {
+  let previousToken = await AsyncStorage.getItem('pushtoken');
+  console.log(previousToken);
+  if (previousToken) {
+    return;
+  }
+  let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+
+  if (status !== 'granted') {
+    return;
+  }
+
+  let token = await Notifications.getExponentPushTokenAsync();
+  axios.post(PUSH_ENDPOINT, { token: { token } });
+  AsyncStorage.setItem('pushtoken', token);
+};
