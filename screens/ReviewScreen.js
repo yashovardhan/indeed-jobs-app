ec83213c595825867092ac8c7a746b6e2b5f63a3
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-13 12:05:22 +0530

    ReviewScreen to show the list of liked jobs

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, Platform, ScrollView, Linking } from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 class ReviewScreen extends Component {
   static navigationOptions = props => {
@@ -21,19 +23,64 @@ class ReviewScreen extends Component {
     };
   };
 
+  renderLikedJobs() {
+    return this.props.likedJobs.map(job => {
+      const { company, formattedRelativeTime, url, latitude, longitude, jobtitle, jobkey } = job;
+
+      const initialRegion = {
+        longitude,
+        latitude,
+        longitudeDelta: 0.045,
+        latitudeDelta: 0.02,
+      };
+
+      return (
+        <Card title={jobtitle} key={jobkey}>
+          <View style={{ height: 200 }}>
+            <MapView
+              scrollEnabled={false}
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android'}
+              initialRegion={initialRegion}
+            />
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{company}</Text>
+              <Text style={styles.italics}>{formattedRelativeTime}</Text>
+            </View>
+            <Button
+              title="Apply Now!"
+              backgroundColor="#03A9F4"
+              onPress={() => Linking.openURL(url)}
+            />
+          </View>
+        </Card>
+      );
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
+        <ScrollView>{this.renderLikedJobs()}</ScrollView>
       </View>
     );
   }
 }
 
-export default ReviewScreen;
+const styles = {
+  detailWrapper: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  italics: {
+    fontStyle: 'italic',
+  },
+};
+
+function mapStateToProps(state) {
+  return { likedJobs: state.likedJobs };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
